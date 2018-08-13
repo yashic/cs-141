4a15e0ad69b3369ee0ba008812aafe57d09ab9b1
yashic <42140098+yashic@users.noreply.github.com>
2018-08-13 14:55:32 +0530

    Add files via upload

--- yashi.sh
+++ yashi.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-echo "what is your name?"
+#!bin/bash
+echo "what is your name"
 read "name"
 mkdir $name
-for i in 1 2 3 4 5
-do 
-touch $name$i.cpp 
-done
+cd $name
+touch $name.cpp
+
